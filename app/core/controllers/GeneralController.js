a4abc6c76aa2fe05eba0ce4f13c0cc72ba63246e
Lucas <lucas-1880@outlook.com>
2021-07-26 18:17:59 -0300

    :art:

--- app/core/controllers/GeneralController.js
+++ app/core/controllers/GeneralController.js
@@ -3,7 +3,7 @@ module.exports = {
     async updateInKannel(dogId) {
         const dog = await Dog.findByPk(dogId)
 
-        if(!dog) return res.status(204).json({error: 'No dogs found'})
+        if (!dog) return res.status(204).json({ error: 'No dogs found' })
 
         dog.inKennel == true ? await Dog.update({ inKennel: false }, { where: { id: dogId } }) :
             await Dog.update({ inKennel: true }, { where: { id: dogId } })
--- app/core/controllers/KennelController.js
+++ app/core/controllers/KennelController.js
@@ -2,20 +2,20 @@ const { Op, where } = require('sequelize')
 const Kennel = require('../models/Kennel')
 const Dog = require('../models/Dog')
 const { findAll } = require('../models/Kennel')
-const {updateInKennel} = require('../controllers/GeneralController')
+const { updateInKennel } = require('../controllers/GeneralController')
 
 module.exports = {
-    async createKennel(req, res){
-        const {name} = req.body
+    async createKennel(req, res) {
+        const { name } = req.body
 
-        const kennel = await Kennel.create({name})
-         
+        const kennel = await Kennel.create({ name })
+
         if (!kennel) return res.status(500).json({ error: 'Kennel was not created properly' })
 
         return res.json(kennel)
     },
 
-    async listAllKennels(req, res){
+    async listAllKennels(req, res) {
         const kennel = await Kennel.findAll()
 
         if (!kennel) return res.status(204).json({ error: 'No kennels' })
@@ -23,9 +23,9 @@ module.exports = {
         return res.json(kennel)
     },
 
-    async findKennelByName(req, res){
-        const {name} = req.body
-        
+    async findKennelByName(req, res) {
+        const { name } = req.body
+
         const kennel = await Kennel.findAll({
             attributes: ['name'],
             where: {
@@ -35,59 +35,59 @@ module.exports = {
             }
         })
 
-        if(!kennel) return res.status(204).json({error: 'No kennel found'})
+        if (!kennel) return res.status(204).json({ error: 'No kennel found' })
 
         return res.json(kennel)
     },
-    
+
     async deleteKennel(req, res) {
-        const {kennelId} = req.params
+        const { kennelId } = req.params
 
         const kennel = await Kennel.findByPk(kennelId)
 
-        if(!kennel) return res.status(204).json({error: 'kennel not existing'})
+        if (!kennel) return res.status(204).json({ error: 'kennel not existing' })
 
         const dogs = await Kennel.getDogs()
 
-        for(dog of dogs){
+        for (dog of dogs) {
             updateInKennel(dog.id)
         }
 
         await kennel.setDogs([])
 
-        await Kennel.destroy({where: {id: KennelId}})
+        await Kennel.destroy({ where: { id: KennelId } })
 
         return res.json(kennel)
 
     },
 
-    async listDogs(req, res){
-        const {kennelId} = req.params
+    async listDogs(req, res) {
+        const { kennelId } = req.params
 
         const kennel = await findAll({
-                where: {
-                    kennelId: kennelId
-                }
+            where: {
+                kennelId: kennelId
             }
+        }
         )
 
-        if(!kennel) return res.status(204).json({error: 'kennel not existing'})
+        if (!kennel) return res.status(204).json({ error: 'kennel not existing' })
 
         const dogs = await kennel.getDogs()
 
-        if(!dogs) return res.status(204).json({error: 'No dogs found'})
+        if (!dogs) return res.status(204).json({ error: 'No dogs found' })
 
         return res.json(dogs)
-        
+
     },
 
     async addDogToKennel(req, res) {
-        const {dogId, kennelId} = req.params
+        const { dogId, kennelId } = req.params
 
         const dog = await Dog.findByPk(dogId)
         const kennel = await Kennel.findByPk(kennelId)
 
-        if(!dog || !kennel) return res.status(404).json({erro: 'not found'})
+        if (!dog || !kennel) return res.status(404).json({ erro: 'not found' })
 
         await kennel.addDog(dog)
 
@@ -97,12 +97,12 @@ module.exports = {
     },
 
     async removeDogFromKennel(req, res) {
-        const {dogId, kennelId} = req.params
+        const { dogId, kennelId } = req.params
 
         const dog = await Dog.findByPk(dogId)
         const kennel = await Kennel.findByPk(kennelId)
 
-        if(!dog || !kennel) return res.status(404).json({erro: 'not found'})
+        if (!dog || !kennel) return res.status(404).json({ erro: 'not found' })
 
         await kennel.removeDog(dog)
 
