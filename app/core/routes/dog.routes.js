ee9a624b715b6a39e88c1ca32c16a9cabbcd0f4a
Lucas <lucas-1880@outlook.com>
2021-07-26 17:59:55 -0300

    Routes/Dog routes :boat:

---
+++ app/core/routes/dog.routes.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const DogController = require('../controllers/DogController')
+
+const dogRoutes = express.Router()
+
+dogRoutes.get('/', DogController.listAllDogs)
+dogRoutes.get('/noKennel', DogController.listNoKennelDogs)
+dogRoutes.get('/findByName', DogController.findDogByName)
+dogRoutes.get('/kennel', DogController.getKennel)
+dogRoutes.delete('/:dogId', DogController.deleteDog)
+dogRoutes.put('/name/:dogId', DogController.updateDogName)
+dogRoutes.put('/age/:dogId', DogController.updateDogAge)
+dogRoutes.put('/image/:dogId', DogController.updateDogImage)
+dogRoutes.put('/weight/:dogId', DogController.updateDogWeight)
+dogRoutes.put('/behavior/:dogId', DogController.updateDogBehavior)
+dogRoutes.put('/entryDate/:dogId', DogController.updateDogEntryDate)
+dogRoutes.put('/:dogId/:kennelId', DogController.updateKennel)
+dogRoutes.post('/', DogController.createDog)
+
+module.exports = dogRoutes
