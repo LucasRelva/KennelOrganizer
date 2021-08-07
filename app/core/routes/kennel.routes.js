c477ceb72c937737fb7930c6d7e0a0b2b177e445
Leonardo <leonardoporto51@gmail.com>
2021-08-07 19:16:26 -0300

    Fixing routes :hammer:

--- app/core/routes/kennel.routes.js
+++ app/core/routes/kennel.routes.js
@@ -9,5 +9,8 @@ kennelRoutes.delete('/:kennelId', KennelController.deleteKennel)//testar depois 
 kennelRoutes.get('/listDogs/:kennelId', KennelController.listDogs)
 kennelRoutes.put('/addDog/:dogId/:kennelId', KennelController.addDogToKennel)
 kennelRoutes.put('/removeDog/:dogId/:kennelId', KennelController.removeDogFromKennel)
+kennelRoutes.put('/name/:kennelId', KennelController.updateKennelName)
+kennelRoutes.get('/:kennelId', KennelController.countDogs)
+
 
 module.exports = kennelRoutes
--- app/core/routes/routes.js
+++ app/core/routes/routes.js
@@ -7,7 +7,9 @@ const routes = express.Router()
 // routes.use(express.static('app/views'))
 // routes.use('/active', express.static('app/views'))
 // routes.use('/completed', express.static('app/views'))
-
+routes.get("/", (req, res) => {
+    return res.render("main.njk", { pageTitle: "TODOS OS CANIS" })
+})
 // Dog routes
 routes.use('/dog', dogRoutes)
 
