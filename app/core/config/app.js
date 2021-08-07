77e6570298088b4bcb10715e52cc48a3bd0ecbba
Leonardo <leonardoporto51@gmail.com>
2021-08-07 19:14:47 -0300

    Template engine config :wrench:

--- app/core/config/app.js
+++ app/core/config/app.js
@@ -1,12 +1,21 @@
 const express = require('express')
 const cors = require('cors')
 const routes = require('../routes/routes')
-const port = 8000
+const path = require('path')
+const port = 8080
 
 require('./dbconnection')
 
 const app = express()
 const server = require('http').createServer(app)
+app.use(express.static(path.join(__dirname + '..', '..', '..', 'views')))
+
+const nunjucks = require("nunjucks")
+nunjucks.configure(path.join(__dirname + '..', '..', '..', 'views'), {
+    autoescape: true,
+    express: app,
+    noCache: true
+});
 
 app.use(express.json())
 app.use(cors())
@@ -16,3 +25,4 @@ app.use(routes)
 server.listen(port, () => {
     console.log('Server running at ' + port)
 })
+
