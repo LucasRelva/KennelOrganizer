1e935c0b2386c130e19c33c85cdffe125587acdc
Leonardo-PRT <leonardporto51@gmail.com>
2021-08-13 16:03:12 -0300

    Emergencial commit

--- app/core/config/app.js
+++ app/core/config/app.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const cors = require('cors')
-const routes = require('../routes/routes')
 const path = require('path')
+const multer = require('multer')
 const port = 8080
 
 require('./dbconnection')
 
 const app = express()
+
 const server = require('http').createServer(app)
 app.use(express.static(path.join(__dirname + '..', '..', '..', 'views')))
 
@@ -17,12 +18,28 @@ nunjucks.configure(path.join(__dirname + '..', '..', '..', 'views'), {
     noCache: true
 });
 
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, path.join(__dirname + '..', '..', '..', '..', 'assets', 'images'))
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + '-' + file.originalname)
+    }
+})
+
+const upload = multer({ storage })
+
+module.exports = upload
+
 app.use(express.json())
 app.use(cors())
+
+const routes = require('../routes/routes')
 
 app.use(routes)
 
 server.listen(port, () => {
     console.log('Server running at ' + port)
 })
+
 
--- app/core/routes/routes.js
+++ app/core/routes/routes.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const dogRoutes = require('./dog.routes')
 const KennelRoutes = require('./kennel.routes')
+const upload = require('../config/app')
+const sharp = require('sharp')
+const fs = require('fs')
 
 const routes = express.Router()
 
-// routes.use(express.static('app/views'))
-// routes.use('/active', express.static('app/views'))
-// routes.use('/completed', express.static('app/views'))
 routes.get("/", (req, res) => {
     return res.render("main.njk", { pageTitle: "TODOS OS CANIS" })
 })
@@ -15,5 +15,25 @@ routes.use('/dog', dogRoutes)
 
 //Kennel ROutes
 routes.use('/kennel', KennelRoutes)
+
+//upload images
+routes.post('/upload', upload.single('photo'), async (req, res) => {
+    const file = req.file
+    sharp.cache(false)
+
+    const [type, extension] = file.mimetype.split('/')
+
+    if (!fs.existsSync(file.destination)) {
+        fs.mkdirSync(file.destination)
+    }
+
+    await sharp(file.path).resize(320, 320, {
+        fit: 'contain'
+    }).toFile(`${file.destination}/${Date.now()}-resized.${extension}`)
+
+    fs.unlinkSync(file.path)
+
+    res.send('deu bom')
+})
 
 module.exports = routes
