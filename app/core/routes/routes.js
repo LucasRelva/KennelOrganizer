bd2cc8359513a15871f0b79f1a360bec6b79f422
Leonardo-PRT <leonardoporto51@gmail.com>
2021-08-18 20:52:32 -0300

    Dog functionalit :sparkles:
    Co-authored-by: Lucas Relva <LucasRelva@users.noreply.github.com>

--- app/core/routes/routes.js
+++ app/core/routes/routes.js
@@ -7,32 +7,20 @@ const fs = require('fs')
 
 const routes = express.Router()
 
-routes.get("/", (req, res) => {
-    return res.render("main.njk", { pageTitle: "TODOS OS CANIS" })
+//frontEnd render routes
+routes.get('/', (req, res) => {
+    return res.render('main.njk', { pageTitle: 'TODOS OS CANIS' })
 })
+
+routes.get('/allDogs', (req, res) => {
+    return res.render('allDogs.njk', { pageTitle: 'TODOS OS CACHORROS' })
+})
+
 // Dog routes
 routes.use('/dog', dogRoutes)
 
 //Kennel ROutes
 routes.use('/kennel', KennelRoutes)
 
-// //upload images
-// routes.post('/upload', upload.single('photo'), async (req, res) => {
-//     const file = req.file
-//     sharp.cache(false)
-//     console.log(req)
-
-//     const [type, extension] = file.mimetype.split('/')
-//     const finalPath = `${file.destination}/${Date.now()}-resized.${extension}`
-
-//     await sharp(file.path).resize(320, 320, {
-//         fit: 'contain'
-//     }).toFile(finalPath)
-
-//     fs.unlinkSync(file.path)
-
-
-//     return res.json(req.body)
-// })
 
 module.exports = routes
