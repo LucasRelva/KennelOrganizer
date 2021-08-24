7e7103ebcf7526caa64b776ba95b1b841ff6a0df
Leonardo-PRT <leonardoporto51@gmail.com>
2021-08-24 19:47:46 -0300

    Front-end delete function :sparkles:
    Co-authored-by: Lucas Relva <LucasRelva@users.noreply.github.com>

--- app/views/scripts/kennel.js
+++ app/views/scripts/kennel.js
@@ -1,6 +1,7 @@
 var kennelForms = document.querySelector('#kennelForm')
 var kennelCards = document.querySelector('.kennelCards')
 
+
 async function createKennel() {
 
     const formData = kennelForms.name.value
@@ -34,9 +35,17 @@ async function listAllKennels() {
         <div id="${kennel.id}" class="card text-center text-white bg-dark " style="max-width: 18rem; margin: 10px; min-width: 18rem;">
             <div id="cardHeader" class="card-header " ondblclick="updateName(${kennel.id})" style="font-size: 25px;">${kennel.name}</div>
             <div class="card-body">
+            <a href ='#' onclick='deleteKennel(${kennel.id})' class = 'trash-icon'> <svg width="1em" height="1em" viewBox="0 0 16 16" 
+            class="bi bi-trash text-danger" fill="currentColor" xmlns="http://www.w3.org/2000/svg" style = 'font-size: 20px;'>
+            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 
+            .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+            <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h
+            2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+            </svg></a>
             <h5 class="card-title"><b>${dogCount}</b> Cachorros</h5>
             <a href="#" class="btn btn-primary" onclick="listKennelDogs(${kennel.id})">Ver canil</a>
             <button data-toggle="modal" data-target="#kennelModalEdit" type="button" class="btn btn-warning" style="margin-rigth: 0" onclick="updateName(${kennel.id})"><img src="https://img.icons8.com/material-outlined/24/000000/edit--v1.png"/></button>
+            </div>
         </div>`
     }
 
@@ -85,20 +94,47 @@ function listKennelDogs(id) {
         kennelCards.innerHTML = ' '
 
         for (dog of kennelDogs) {
-            kennelCards.innerHTML += `
-            <div id="${dog.id}" class="card text-center text-white bg-dark " style="max-width: 14rem; margin: 10px; min-width: 14rem;">
-                <img src="/images/${dog.image}" class="card-img-top" alt="...">
-                <div class="card-header" style="font-size: 25px">Cachorro</div>
-                <div class="card-body bg-dark ">
-                <h5 class="card-title">${dog.name}</h5>
-                <h5 class="card-title">${dog.behavior}</h5>
-                <p class="card-text">${dog.entryDate}</p>
-                <a href="#" class="btn btn-primary">Ver detalhes</a>           
-          </div>`
+            kennelCards.innerHTML += `<div id="${dog.id}" class="card text-center text-white bg-dark " style="max-width: 14rem; margin: 10px; min-width: 14rem;">
+            <img src="/images/${dog.image}" class="card-img-top" alt="...">
+              <div class="card-header" style="font-size: 25px">${dog.name}</div>
+              <div class="card-body bg-dark ">
+              <h5 class="card-title">${dog.behavior}</h5>
+              <p class="card-text">${dog.entryDate}</p>
+              <a href ='#' onclick='deleteDog(${dog.id})' class = 'trash-icon'> <svg width="1em" height="1em" viewBox="0 0 16 16" 
+                                class="bi bi-trash text-danger" fill="currentColor" xmlns="http://www.w3.org/2000/svg" style = 'font-size: 20px;'>
+                                <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 
+                                .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                                <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h
+                                2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+                              </svg></a>
+                </div>
+              <a href="#" class="btn btn-primary">Ver detalhes</a>`
         }
     })
 
     return
+}
+
+function deleteKennel(id) {
+
+    if (!confirm('Tem certeza que deseja fazer isso?')) return
+
+    axios.delete('/kennel/' + id).then(() => {
+        listAllKennels()
+    }).catch(err => {
+        console.log('Error while deleting a kennel ' + err);
+    })
+}
+
+function deleteDog(id) {
+
+    if (!confirm('Tem certeza que deseja fazer isso?')) return
+
+    axios.delete('/dog/' + id).then(() => {
+        listKennelDogs()
+    }).catch(err => {
+        console.log('Error while deleting a dog ' + err);
+    })
 }
 
 listAllKennels()
