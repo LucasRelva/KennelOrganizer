10111ddcfb882539a469a3e738c6f859e1844b53
Lucas <lucas-1880@outlook.com>
2021-09-14 15:54:24 -0300

    removing comments :mute:

--- app/core/routes/kennel.routes.js
+++ app/core/routes/kennel.routes.js
@@ -5,7 +5,7 @@ const kennelRoutes = express.Router()
 kennelRoutes.post('/', KennelController.createKennel)
 kennelRoutes.get('/', KennelController.listAllKennels)
 kennelRoutes.get('/find', KennelController.findKennelByName)
-kennelRoutes.delete('/:kennelId', KennelController.deleteKennel)//testar depois de adicionar um cachorro ao canil
+kennelRoutes.delete('/:kennelId', KennelController.deleteKennel)
 kennelRoutes.get('/listDogs/:kennelId', KennelController.listDogs)
 kennelRoutes.put('/addDog/:dogId/:kennelId', KennelController.addDogToKennel)
 kennelRoutes.put('/removeDog/:dogId/:kennelId', KennelController.removeDogFromKennel)
